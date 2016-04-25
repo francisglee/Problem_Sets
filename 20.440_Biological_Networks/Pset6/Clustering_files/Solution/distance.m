73bf893de271f8d228f1adeeafc552df59a7a376
Francis Lee <francis.g.lee@gmail.com>
2016-04-25 16:35:48 -0400

    Create distance.m

---
+++ 20.440_Biological_Networks/Pset6/Clustering_files/Solution/distance.m
@@ -0,0 +1,9 @@
+%% Distance function
+function fun = distance(dist)
+switch dist
+    case 'Euclidean'
+        fun = @(x, y) sqrt(sum((x - y).^2));
+    case 'CityBlock'
+        fun = @(x, y) sum(abs(x - y)); 
+end
+
